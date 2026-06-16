f19b96f06d0733c9aab735f859e0fcb5792a1b9b
Simon R. Lincoln <oss@codeability.co.uk>
2026-06-16 13:26:16 +0100

    refactor(ota): Simplify logging methods in _NullLogger and enhance UART interface validation in OTAManager

--- packages/device/lib/otampy/ota.py
+++ packages/device/lib/otampy/ota.py
@@ -10,56 +10,65 @@ from time import time
 class _NullLogger:
     __slots__ = ()
 
-    def _log(self, level, *args):
-        print("[{}] [{:8}] {}".format(time(), level, " ".join(map(str, args))))
+    def _log(self, level, msg):
+        print("[" + str(time()) + "] [" + level + "] " + msg)
 
-    def debug(self, *args, **kwargs):
-        self._log("DEBUG", *args)
+    def debug(self, msg):
+        self._log("DEBUG", msg)
 
-    def info(self, *args, **kwargs):
-        self._log("INFO", *args)
+    def info(self, msg):
+        self._log("INFO", msg)
 
-    def warning(self, *args, **kwargs):
-        self._log("WARNING", *args)
+    def warning(self, msg):
+        self._log("WARNING", msg)
 
-    def error(self, *args, **kwargs):
-        self._log("ERROR", *args)
+    def error(self, msg):
+        self._log("ERROR", msg)
 
-    def critical(self, *args, **kwargs):
-        self._log("CRITICAL", *args)
+    def critical(self, msg):
+        self._log("CRITICAL", msg)
 
-    def exception(self, *args, **kwargs):
-        self._log("ERROR", *args)
+    def exception(self, msg):
+        self._log("ERROR", msg)
 
 
 class OTAManager:
     """The OTAManager class provides methods for performing over-the-air updates on devices."""
 
-    def __init__(self, uart, config=None, logger=None):
+    def _has_uart_interface(self, uart):
         try:
-            from urst import Urst
-
-            """Initializes a new OTAManager object."""
-            # Initialize URST with the UART object
-            self.transport = Urst(uart)
-
-            # Initialise the logger and configuration
-            if config is None:
-                config = {}
-                config["LOG_LEVEL"] = "DEBUG"
-                config["LOG_FILE"] = "/logs/ota.log"
-            self.config = config
-
-            self.logger = logger if logger is not None else _NullLogger()
-
-            # Initialise the UART connection to the device
-            self.uart = uart
-
-        except (ImportError, AttributeError):
-            print(
-                "Not running on MicroPython, or machine module not available."
+            return (
+                callable(uart.write)
+                and callable(uart.read)
+                and callable(uart.any)
             )
-            print("Falling back to a mock/simulated serial for demonstration.")
+        except AttributeError:
+            return False
+
+    def __init__(self, uart, config=None, logger=None):
+        from urst import Urst
+
+        """Initializes a new OTAManager object."""
+        # Initialise the logger and configuration
+        if config is None:
+            config = {}
+            config["LOG_LEVEL"] = "DEBUG"
+            config["LOG_FILE"] = "/logs/ota.log"
+        self.config = config
+
+        self.logger = logger if logger is not None else _NullLogger()
+
+        # Initialise the UART connection to the device
+        self.uart = uart
+        if self._has_uart_interface(uart):
+            self.transport = Urst(uart)
+        else:
+            self.logger.warning(
+                "UART object is not available or does not provide the expected interface."
+            )
+            self.logger.debug(
+                "Falling back to a mock/simulated serial for demonstration."
+            )
 
             # This part is just so the script can be 'run' on desktop without errors
             class MockUART:
@@ -72,14 +81,14 @@ class OTAManager:
                 def any(self):
                     return 0
 
-            self.transport = Urst(MockUART())
+            self.uart = MockUART()
+            self.transport = Urst(self.uart)
 
     def check_for_update(self, callback):
         """Checks for presence of update request flag file. If found, runn the application callback function."""
         self.logger.debug(
             "Checking for presence of update request flag file..."
         )
-        pass
 
     def ready_for_update(self):
         """Resets the device so that boot.py gets run."""

db4c2eed88371c28108feacd152d9734e4a64d01
Simon R. Lincoln <oss@codeability.co.uk>
2026-06-18 20:18:56 +0100

    feat(utils rx): Add placeholder rx tool

---
+++ packages/utils/src/rx.py
@@ -0,0 +1,9 @@
+"""
+Rx monitors data received and decodes any URST frames. All data is presented to allow easier serial data diagnostics.
+"""
+
+from urst import Urst
+
+# TODO: If received data is an URST frame decode it and output as: {Payload}\n[FT:{FrameType}, SN:{SeqNumber}, CRC:{CRC}]\n
+
+# TODO: Output serial data directly if it's NOT URST protocol frame

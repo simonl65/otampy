9c1054c1be47a00e09390cd64267a931206a0cf8
Simon R. Lincoln <oss@codeability.co.uk>
2026-06-29 14:09:32 +0100

    style(examples): unify ota and otalogger imports from package root in boot.py example

--- packages/device/examples/boot.py
+++ packages/device/examples/boot.py
@@ -1,14 +1,13 @@
 import config
 from log_to_file import Logger  # type: ignore
 from machine import UART, Pin  # type: ignore
-from urst import Urst  # type: ignore
 from utime import sleep_ms  # type: ignore
 
 from otampy import OTA, OTALogger  # type: ignore
 
 logger = Logger(
     config.LOG_FILE, "boot.py", level=config.LOG_LEVEL
-) or OTALogger(config.LOG_FILE, level=config.LOG_LEVEL)
+) or OTALogger(level=config.LOG_LEVEL)
 
 uart = UART(
     config.OTA_PORT,
@@ -18,20 +17,17 @@ uart = UART(
 )
 print(uart)
 
-urst = Urst(uart)
-
-# Initialize OTA in boot mode and use the fallback logger if needed.
 ota = OTA(uart, config=config, logger=logger)
 
 led = Pin("LED", Pin.OUT)
 
 led.on()
 
-urst.send(b"BOOTING...\n")
+ota._core.transport.send(b"BOOTING...\n")
 
 # Check for an update request flag before continuing to the main application.
 ota.boot()
 
-urst.send(b"Loading MAIN...\n")
+ota._core.transport.send(b"Loading MAIN...\n")
 
 sleep_ms(200)

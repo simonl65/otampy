9eda25d2a78fdddc12f8b8ae73cb2846c83da0e7
Simon R. Lincoln <oss@codeability.co.uk>
2026-06-29 19:06:41 +0100

    refactor(ota): Remove debug logging from OTA class methods

--- packages/device/lib/otampy/ota.py
+++ packages/device/lib/otampy/ota.py
@@ -11,18 +11,15 @@ class OTA:
 
     def __init__(self, uart, config=None, logger=None):
         self._core = OTACore(uart, config, logger)
-        self._core.logger.debug("OTA Init")
 
     def boot(self, callback=None):
         """
         Call from boot.py. Checks for any pending updates and applies them.
         """
-        self._core.logger.debug("OTA boot run")
         _boot.run(self._core, callback)
 
     def poll(self, callback=None):
         """
         Call from main.py loop. Polls UART transport for incoming OTA commands.
         """
-        self._core.logger.debug("OTA manager poll")
         _manager.poll(self._core, callback)

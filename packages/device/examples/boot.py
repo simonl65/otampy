e39f8a8610ed638b3d5e67d710bcdf318f186982
Simon R. Lincoln <oss@codeability.co.uk>
2026-06-30 19:46:20 +0100

    chore(boot): update logger debug messages to use string format and remove unnecessary sleep

--- packages/device/examples/boot.py
+++ packages/device/examples/boot.py
@@ -27,13 +27,13 @@ led = Pin("LED", Pin.OUT)
 
 led.on()
 
-logger.debug(b"BOOTING...\n")
+logger.debug("BOOTING...")
+
 
 # Check for an update request flag before continuing to the main application.
 ota.boot()
 
-logger.debug(b"Loading MAIN...\n")
-
-sleep_ms(200)
 
 led.off()
+
+logger.debug("Loading MAIN...")

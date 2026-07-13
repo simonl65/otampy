33167d5fe2b44b220cd71525483794e40f009ba7
Simon R. Lincoln <oss@codeability.co.uk>
2026-07-13 21:10:36 +0100

    feat(config): Defult log-level to ERROR

--- src/otampy/device/examples/configota.example.py
+++ src/otampy/device/examples/configota.example.py
@@ -1,18 +1,14 @@
-"""
-RENAME THIS FILE TO configota.py AND EDIT THE VALUES BELOW TO MATCH YOUR ENVIRONMENT
-"""
-
 # Used by the examples when optional log-to-file support is installed.
-LOG_LEVEL = "DEBUG"  # NONE, DEBUG, INFO, WARNING, ERROR, CRITICAL, ALWAYS
+LOG_LEVEL = "ERROR"  # NONE, DEBUG, INFO, WARNING, ERROR, CRITICAL, ALWAYS
 LOG_FILE = "/ota.log"
 
 OTA_PORT = 1
 OTA_TX_PIN = 4
 OTA_RX_PIN = 5
-OTA_BAUDRATE = 57600  # 115200 is unreliable wirth old XBee-Pro's
+OTA_BAUDRATE = 57600  # 115200 is unreliable with old XBee-Pro's
 OTA_TIMEOUT_MS = 5000
 
 # =============================================================================
-# BEST PRACTICE: DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING
+#       DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING
 # =============================================================================
 UPDATE_REQUEST_FLAG_FILE = "update_requested.flag"
